78585f7e549b52ef8dcdac98a8bbc7ca62cb01a0
codeph0enix <ryanabeagle@gmail.com>
2026-01-18 19:31:59 -0700

    Added script for installing recommended packages on a fresh hyprland install.

---
+++ install-packages.sh
@@ -0,0 +1,2 @@
+sudo pacman -Syu firefox dhcpcd htop otf-font-awesome hyprshot hyprlock hypridle brightnessctl gnome-keyring noto-fonts-emoji os-prober 
+sudo yay -Syu joplin-desktop powershell-bin spotify 
